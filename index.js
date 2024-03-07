635de9201ee7416f32db18a861db880462073f18
puneeth037h <punithshanakanahalli@gmail.com>
2024-03-08 01:05:48 +0530

    cat number

--- index.js
+++ index.js
@@ -26,6 +26,29 @@ db.connect((err) => {
 // Express Middleware
 app.use(express.json());
 
+//get category number
+app.get('/categorynumber', (req, res) => {
+  // Construct a simple SELECT query to retrieve all data from the 'category' table
+  let query = 'SELECT COUNT(*) AS noOfCategories FROM category';
+
+  // Log the SQL query for debugging purposes
+  console.log(query);
+
+  // Execute the database query
+  db.query(query, (err, results) => {
+    if (err) {
+      // Handle database error
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+
+    // Log the results to the console for debugging
+    console.log(results);
+
+    // Send the results as a JSON response to the client
+    res.json(results);
+  });
+});
 //search
 app.post('/search', (req, res) => {
   // Assuming productName is coming from the client-side
